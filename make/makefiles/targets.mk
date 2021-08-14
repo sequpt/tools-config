a8d2960415ad715d613ee18d39bc6f7507d170a3
sequpt <sequpt@protonmail.com>
2021-08-14 18:58:24 +0000

    Refactor Makefile
    - Add common expected variables(`DESTDIR`, prefix`, `libdir`, etc).
    - Add `header.pc.in` template and variables/targets to process it.
    - Update targets and reorganise them by themes.
    - Add error message if `BUILD_MODE` is neither `debug` nor `release`.
    - Remove the need to manually add `SRC_DIRS :=` for header project.

--- make/makefiles/targets.mk
+++ make/makefiles/targets.mk
@@ -9,6 +9,10 @@ ifneq ($(PROJECT_TYPE),$(findstring $(PROJECT_TYPE),header program shared static
     $(error PROJECT_TYPE must be one of [header, program, shared, static])
 endif
 
+ifneq ($(BUILD_MODE),$(findstring $(BUILD_MODE),debug release))
+    $(error BUILD_MODE must be one of [debug, release])
+endif
+
 # Build project and tests(if there is a test folder)
 .PHONY: all
 all:: $(PROJECT_DEPS) $(PROJECT_TYPE) $(TEST_TARGET)
@@ -16,12 +20,103 @@ all:: $(PROJECT_DEPS) $(PROJECT_TYPE) $(TEST_TARGET)
 # Compile *.c files
 $(OBJ_PATH)/%.o: %.c $(OBJ_DEPS)
 	$(COMPILE)
+################################################################################
+# HEADER PROJECT
+################################################################################
+ifeq ($(PROJECT_TYPE),header)
+# Nothing to build for header only project(tests are build with the `test`
+# target).
+SRC_DIRS :=
+.PHONY: header
+header::;
+endif
+################################################################################
+# HEADER/LIBRARY PROJECT COMMON
+################################################################################
+ifeq ($(PROJECT_TYPE),$(findstring $(PROJECT_TYPE),header shared static))
+# Install header files(*.h)
+INSTALL_DEPS := install_header
+.PHONY: install_header
+install_header:: $(PKG_HEADER_TARGET) | $(HEADER_INSTALL_PATH) $(PKG_PC_INSTALL_PATH)
+	cp -r $(INC_PATH)/. $(HEADER_INSTALL_PATH)
+	mv $(PKG_PC_NAME) $(PKG_PC_INSTALL_PATH)
 
-# Analyze code with various tools
-.PHONY: analyze
-analyze:: | $(SCANBUILD_PATH) $(CPPCHECK_PATH)
-	$(SCANBUILD)
-	$(CPPCHECK)
+# *.pc.in => *.pc
+$(PKG_HEADER_TARGET)::
+	$(PKG_PC_IN)
+
+# Uninstall header files(*.h/*.pc)
+UNINSTALL_DEPS := uninstall_header
+.PHONY: uninstall_header
+uninstall_header::
+	rm -r $(HEADER_INSTALL_PATH)
+	rm $(PKG_PC_INSTALL_PATH)/$(PKG_PC_NAME)
+endif
+################################################################################
+# LIBRARY PROJECT
+################################################################################
+ifeq ($(PROJECT_TYPE),$(findstring $(PROJECT_TYPE),shared static))
+# Install library files(*.a/*.so)
+INSTALL_DEPS := install_lib
+.PHONY: install_lib
+install_lib:: install_header | $(LIB_INSTALL_PATH)
+	cp -ar $(BUILD_PATH)/* $(LIB_INSTALL_PATH)
+	find $(EXT_LIB_PATH) -type f,l -exec cp -at $(LIB_INSTALL_PATH) {} +
+
+# Build shared library(*.so)
+.PHONY: shared
+shared:: $(LIB_SHARED_TARGET)
+
+# *.o => *.so.*
+$(LIB_SHARED_TARGET): $(OBJ_FILES) | $(BUILD_PATH)
+	$(LINK_SHARED)
+	ln -sf $(LIB_SHARED_XYZ) $(LIB_SHARED_X_PATH)
+	ln -sf $(LIB_SHARED_X) $(LIB_SHARED_PATH)
+
+# Build static library(*.a)
+.PHONY: static
+static:: $(LIB_STATIC_TARGET)
+
+# *.o => *.a
+$(LIB_STATIC_TARGET): $(OBJ_FILES) | $(BUILD_PATH)
+	$(ARCHIVE)
+
+# Uninstall library files(*.a/*.so.*/*.h/*.pc)
+UNINSTALL_DEPS := uninstall_lib
+.PHONY: uninstall_lib
+uninstall_lib:: uninstall_header
+	rm -r $(LIB_INSTALL_PATH)
+endif
+################################################################################
+# PROGRAM PROJECT
+################################################################################
+# Build program
+ifeq ($(PROJECT_TYPE),program)
+.PHONY: program
+program:: $(PROGRAM_TARGET)
+
+# *.o => executable
+$(PROGRAM_TARGET): $(OBJ_FILES) | $(BUILD_PATH)
+	$(LINK_PROGRAM)
+
+# Run program
+RUN_CMD = $(PROGRAM_PATH)
+.PHONY: run
+run:: $(RUN_DEPS)
+	$(RUN_CMD)
+endif
+################################################################################
+# TEST
+################################################################################
+# Build tests if there is a test folder
+ifneq ($(TEST_TARGET),empty_target)
+.PHONY: $(TEST_TARGET)
+$(TEST_TARGET):: test
+.PHONY: test
+test::
+	$(MAKE) -C $(TEST_PATH) all CC=$(CC) BUILD_MODE=$(BUILD_MODE) \
+	MAKEFILE_PATH=../$(MAKEFILE_PATH)
+endif
 
 # Run tests(they must be build first)
 .PHONY: check
@@ -29,19 +124,18 @@ check::
 	$(MAKE) -C $(TEST_PATH) run BUILD_MODE=$(BUILD_MODE) \
 	MAKEFILE_PATH=../$(MAKEFILE_PATH)
 
-# Remove build files
-ifneq ($(TEST_TARGET),empty_target)
-CLEAN_TEST := clean_test
-endif
-CLEAN_CMD := rm -rf build
-.PHONY: clean
-clean:: $(CLEAN_TEST)
-	$(CLEAN_CMD)
-
 # Remove test build files
 .PHONY: clean_test
 clean_test::
 	$(MAKE) -C $(TEST_PATH) clean MAKEFILE_PATH=../$(MAKEFILE_PATH)
+################################################################################
+# CODE ANALYSIS/COVERAGE - DOC
+################################################################################
+# Analyze code with various tools
+.PHONY: analyze
+analyze:: | $(SCANBUILD_PATH) $(CPPCHECK_PATH)
+	$(SCANBUILD)
+	$(CPPCHECK)
 
 # Code coverage tools
 .PHONY: cov
@@ -58,86 +152,37 @@ $(LCOV_PATH)/%.info: $(OBJECT_PATH)/%.o | $(LCOV_PATH)
 .PHONY: doc
 doc:: | $(DOXYGEN_PATH)
 	$(DOXYGEN)
-
-.PHONY: empty_target
-empty_target::;
-
-ifeq ($(PROJECT_TYPE),header)
-.PHONY: header
-header::;
+################################################################################
+# CLEAN/REBUILD/INSTALL/UNINSTALL
+################################################################################
+# Remove build files
+ifneq ($(TEST_TARGET),empty_target)
+CLEAN_TEST := clean_test
 endif
-
-# Install everything
-.PHONY: install
-install:: all install_header install_lib
-
-# Install header files
-.PHONY: install_header
-install_header:: all | $(HEADER_INSTALL_PATH)
-	cp -r $(INC_PATH)/. $(HEADER_INSTALL_PATH)
-
-# Install library files
-.PHONY: install_lib
-install_lib:: all | $(LIB_INSTALL_PATH)
-	cp -ar $(BUILD_PATH)/* $(LIB_INSTALL_PATH)
-	find $(EXTERNAL_LIB_PATH) -type f,l -exec cp -at $(LIB_INSTALL_PATH) {} +
-
-# Build program
-ifeq ($(PROJECT_TYPE),program)
-.PHONY: program
-program:: $(PROGRAM_TARGET)
-
-$(PROGRAM_TARGET): $(OBJ_FILES) | $(BUILD_PATH)
-	$(LINK_PROGRAM)
-endif
+CLEAN_CMD = rm -rf $(BUILD_ROOT_PATH)
+.PHONY: clean
+clean:: $(CLEAN_TEST)
+	$(CLEAN_CMD)
 
 # Remove existing build files and build again
 .PHONY: rebuild
 rebuild:: clean all
 
-# Build project if not already built and run it
-ifeq ($(PROJECT_TYPE),program)
-RUN_CMD = $(PROGRAM_PATH)
-.PHONY: run
-run:: $(RUN_DEPS)
-	$(RUN_CMD)
-endif
-
-ifeq ($(PROJECT_TYPE),$(findstring $(PROJECT_TYPE),shared static))
-# Build shared library(.so)
-.PHONY: shared
-shared:: $(LIB_SHARED_TARGET)
-
-$(LIB_SHARED_TARGET): $(OBJ_FILES) | $(BUILD_PATH)
-	$(LINK_SHARED)
-	ln -sf $(LIB_SHARED_XYZ) $(LIB_SHARED_X_PATH)
-	ln -sf $(LIB_SHARED_X) $(LIB_SHARED_PATH)
-
-# Build static library(.a)
-.PHONY: static
-static:: $(LIB_STATIC_TARGET)
-
-$(LIB_STATIC_TARGET): $(OBJ_FILES) | $(BUILD_PATH)
-	$(ARCHIVE)
-endif
-
-# Build tests if there is a test folder
-ifneq ($(TEST_TARGET),empty_target)
-.PHONY: $(TEST_TARGET)
-$(TEST_TARGET):: test
-.PHONY: test
-test::
-	$(MAKE) -C $(TEST_PATH) all CC=$(CC) BUILD_MODE=$(BUILD_MODE) \
-	MAKEFILE_PATH=../$(MAKEFILE_PATH)
-endif
+# Install everything
+.PHONY: install
+install:: $(INSTALL_DEPS)
 
 # Uninstall everything
 .PHONY: uninstall
-uninstall::
-	rm -rf $(HEADER_INSTALL_PATH)
-	rm -rf $(LIB_INSTALL_PATH)
-
-# Targets to create output directories
+uninstall:: $(UNINSTALL_DEPS)
+################################################################################
+# MISC
+################################################################################
+# Targets depending on the existence of an optional file/directory are
+# redirected here to avoid make failing with `No rule to make target`.
+.PHONY: empty_target
+empty_target::;
+# Targets to create output directories when they are needed but don't exist yet.
 # mkdir -p: No error if directory exists and make parent directories as needed
 $(ANALYSIS_PATH):       ;mkdir -p $(ANALYSIS_PATH)
 $(BUILD_PATH):          ;mkdir -p $(BUILD_PATH)
@@ -146,15 +191,16 @@ $(DEP_PATH):            ;mkdir -p $(DEP_PATH)
 $(DOXYGEN_PATH):        ;mkdir -p $(DOXYGEN_PATH)
 $(GCOV_PATH):           ;mkdir -p $(GCOV_PATH)
 $(HEADER_INSTALL_PATH): ;mkdir -p $(HEADER_INSTALL_PATH)
-$(LIB_INSTALL_PATH):    ;mkdir -p $(LIB_INSTALL_PATH)
 $(LCOV_PATH):           ;mkdir -p $(LCOV_PATH)
+$(LIB_INSTALL_PATH):    ;mkdir -p $(LIB_INSTALL_PATH)
 $(OBJ_PATH):            ;mkdir -p $(OBJ_PATH)
+$(PKG_PC_INSTALL_PATH): ;mkdir -p $(PKG_PC_INSTALL_PATH)
 $(SCANBUILD_PATH):      ;mkdir -p $(SCANBUILD_PATH)
 $(VALGRIND_PATH):       ;mkdir -p $(VALGRIND_PATH)
 # Make each *.d file a target so make won't fail if the file doesn't exist
 $(DEP_FILES):
 # Only include *.d files if needed
-ifneq ($(MAKECMDGOALS),$(findstring $(PROJECT_TYPE),clean doc header uninstall))
+ifeq ($(MAKECMDGOALS),$(findstring $(PROJECT_TYPE),program shared static))
     include $(wildcard $(DEP_FILES))
 endif
 # Delete files generated by a target which failed(prevent corrupted files)
